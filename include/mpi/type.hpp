993b8ed2d2fb40678d5d21eaf43a4a995f674e32
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-09-04 21:50:30 -0400

    add mpi type traits

---
+++ include/mpi/type.hpp
@@ -0,0 +1,53 @@
+///
+///
+///
+///
+///
+
+#ifndef TYPE_HPP
+#define TYPE_HPP
+
+#pragma once
+#include <mpi.h>
+
+namespace mpi_type
+{
+template <typename T>
+struct mpi_type_traits;
+
+template <>
+struct mpi_type_traits<uint8_t>
+{
+  static MPI_Datatype get() { return MPI_UINT8_T; }
+};
+
+template <>
+struct mpi_type_traits<uint16_t>
+{
+  static MPI_Datatype get() { return MPI_UINT16_T; }
+};
+
+template <>
+struct mpi_type_traits<int>
+{
+  static MPI_Datatype get() { return MPI_INT; }
+};
+template <>
+struct mpi_type_traits<float>
+{
+  static MPI_Datatype get() { return MPI_FLOAT; }
+};
+template <>
+struct mpi_type_traits<double>
+{
+  static MPI_Datatype get() { return MPI_DOUBLE; }
+};
+template <>
+struct mpi_type_traits<long>
+{
+  static MPI_Datatype get() { return MPI_LONG; }
+};
+
+}  // end of namespace mpi_type
+
+#endif  // endif
