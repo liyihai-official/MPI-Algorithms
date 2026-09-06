1bce83c83017416ee5f668b120437d2288d0dbbd
liyihai-official <liyihai-official@users.noreply.github.com>
2026-09-06 05:29:06 +0000

    deploy: af37f4c0f2ba99ba51520aeda9b40f8cda81e9d0

--- bruck__Alltoall_8cc.js
+++ bruck__Alltoall_8cc.js
@@ -4,7 +4,7 @@ var bruck__Alltoall_8cc =
     [ "DIM_Y", "bruck__Alltoall_8cc.html#a5fa9a687139f39c5033eb566a9e7b6e1", null ],
     [ "DIM_Z", "bruck__Alltoall_8cc.html#a3ee1669e4235e65a86c90dfecbff5387", null ],
     [ "size_type", "bruck__Alltoall_8cc.html#a8560ac5527cd0ed38d71e0e58acc8021", null ],
-    [ "value_type", "bruck__Alltoall_8cc.html#a099c90519d34c2dbab58e4a771aa6f25", null ],
+    [ "value_type", "bruck__Alltoall_8cc.html#afb8172682a009a8f61f7787f41448e6d", null ],
     [ "main", "bruck__Alltoall_8cc.html#a3c04138a5bfe5d72780bb7e82a18e627", null ],
     [ "SEED", "bruck__Alltoall_8cc.html#aae732a9029a5aba6be7223976e4d6a35", null ]
 ];
--- search/all_11.js
+++ search/all_11.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['value_5ftype_0',['value_type',['../classmulti__array_1_1array.html#ab06ee6e6cc134438c657e068f81a484b',1,'multi_array::array::value_type'],['../bruck__Alltoall_8cc.html#a099c90519d34c2dbab58e4a771aa6f25',1,'value_type:&#160;bruck_Alltoall.cc']]]
+  ['value_5ftype_0',['value_type',['../classmulti__array_1_1array.html#ab06ee6e6cc134438c657e068f81a484b',1,'multi_array::array::value_type'],['../bruck__Alltoall_8cc.html#afb8172682a009a8f61f7787f41448e6d',1,'value_type:&#160;bruck_Alltoall.cc']]]
 ];
